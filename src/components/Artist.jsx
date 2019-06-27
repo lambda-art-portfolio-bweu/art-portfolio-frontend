f92e17e4e29da37b71546e50f90ace5a388648c9
iykekings <iykekings36@gmail.com>
2019-06-27 11:08:14 +0100

    Add more features to Artist component

--- src/components/Artist.jsx
+++ src/components/Artist.jsx
@@ -15,6 +15,8 @@ export default function Artist(props) {
       cover={
         <Carousel>
           <img src={artist.pictureUrl} alt="" />
+          <img src={artist.pictureUrl} alt="" />
+          <img src={artist.pictureUrl} alt="" />
         </Carousel>
       }
     >
