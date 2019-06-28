1b76ac4eb85e1c9f2014b3b000b8ae91b8d3168f
ruihildt <ruihildt@protonmail.com>
2019-06-28 13:19:51 +0200

    Remove post metadata from PostCard

--- src/components/postslist/PostCard.jsx
+++ src/components/postslist/PostCard.jsx
@@ -4,15 +4,10 @@ import styled from "styled-components";
 import { Link } from 'react-router-dom';
 
 export default function PostCard(props) {
-  const { post } = props
 
     return (
       // <Link to={`${props.username}`}>
       <StyledCol span={12}>
-        <Button onClick={() => props.deletePost(post.id)}>Delete</Button>
-        <p>{post.name}</p>
-        <p>{post.heart}</p>
-        <p>{post.description}</p>
         <img src={props.post.pictureUrl} alt="" />
       </StyledCol>
       // </Link>
