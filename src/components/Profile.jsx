89cba6273b4d7dfb677dde1654d5daedd3855494
ruihildt <ruihildt@protonmail.com>
2019-07-04 19:20:38 +0200

    Add modal to edit profile picture on click

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,61 +1,102 @@
-import React from 'react';
-import { Row, Col, Typography } from 'antd';
+import React, { useState } from 'react';
+import { Row, Col, Typography, Modal, Input } from 'antd';
 import styled from 'styled-components';
+
 import AddPostModal from './AddPostModal';
 import PostsGrid from './postsblock/PostsGrid';
+import camera from '../assets/camera.svg';
 
-// Style is under the component
 const { Title, Paragraph } = Typography;
 
 export default function Profile(props) {
+  const [visible, setVisible] = useState(false);
+  const [imageURL, setImageURL] = useState('');
 
   const handleDescription = str => {
     props.updateArtist(props.artist.id, { artistDescription: str })
-  }
-
-  const handleName = str => {
-    props.updateArtist(props.artist.id, { artistName: str })
   };
 
+  const handleName = str => {
+    props.updateArtist(props.artist.id, { artistName: str });
+  };
+
+  const handleURL = target => {
+    setImageURL(target.value);
+  };
+
+  const showModal = () => {
+    setVisible(true);
+  };
+
+  const handleOk = e => {
+    props.updateArtist(props.artist.id, { profilePictureUrl: imageURL });
+    setVisible(false);
+  };
+
+  const handleCancel = e => {
+    console.log(e);
+    setVisible(false);
+  };
+
+
   return (
-      <MainContainer>
-        <ArtistinfoRow type="flex" justify="center">
-          <StyledImgCol>
-            <StyledImg
+    <MainContainer>
+      <ArtistinfoRow type="flex" justify="center">
+        <StyledImgCol>
+          <PictureContainer>
+            <ProfileImg
               src={props.artist.profilePictureUrl}
               alt={props.artist.artistName}
+              onClick={showModal}
             />
-          </StyledImgCol>
-          <StyledInfoCol>
-            <Title editable={{ onChange: handleName }}>
-              {props.artist.artistName}
-            </Title>
-            <Paragraph editable={{ onChange: handleDescription }}>
-              {props.artist.artistDescription}
-            </Paragraph>
-          </StyledInfoCol>
-          <StyledButtonCol>
-            <AddPostModal id={props.artist.id} />
-          </StyledButtonCol>
-        </ArtistinfoRow>
-        
-        <Row type="flex" justify="center">
-          <StyledButtonsCol />
-        </Row>
+            <CameraImg
+              src={camera}
+              alt={props.artist.artistName}
+              onClick={showModal}
+            />
+          </PictureContainer>
+          <Modal
+            title="Update image"
+            visible={visible}
+            onOk={handleOk}
+            onCancel={handleCancel}
+          >
+            <Input
+              placeholder="Add your new image URL"
+              id="url"
+              onChange={e => handleURL(e.target)}
+            />
+          </Modal>
+        </StyledImgCol>
+        <StyledInfoCol>
+          <Title editable={{ onChange: handleName }}>
+            {props.artist.artistName}
+          </Title>
+          <Paragraph editable={{ onChange: handleDescription }}>
+            {props.artist.artistDescription}
+          </Paragraph>
+        </StyledInfoCol>
+        <StyledButtonCol>
+          <AddPostModal id={props.artist.id} />
+        </StyledButtonCol>
+      </ArtistinfoRow>
 
-        <StyledPostsRow type="flex" justify="center">
-          <Col>
-            <PostsGrid
-              id={props.artist.id}
-            />
-          </Col>
-        </StyledPostsRow>
-        </MainContainer>
+      <Row type="flex" justify="center">
+        <StyledButtonsCol />
+      </Row>
+
+      <StyledPostsRow type="flex" justify="center">
+        <Col>
+          <PostsGrid id={props.artist.id} />
+        </Col>
+      </StyledPostsRow>
+    </MainContainer>
   );
 }
 const MainContainer = styled.div`
   width: 100%;
 `;
+
 const ArtistinfoRow = styled(Row)`
   align-items: center;
   padding: 2rem 0;
@@ -63,11 +104,33 @@ const ArtistinfoRow = styled(Row)`
 const StyledImgCol = styled(Col)`
   width: 20%;
 `;
-const StyledImg = styled.img`
-  width: 100%;
+// Image overlay
+const PictureContainer = styled.div`
   max-width: 160px;
+  position: relative;
+  display: inline-block;
+`;
+const ProfileImg = styled.img`
+  width: 100%;
   border-radius: 90%;
 `;
+const CameraImg = styled.img`
+  width: 100%;
+  border-radius: 90%;
+  opacity: 0;
+  padding: 3rem;
+  background-color: hsla(0, 0%, 100%, 0.7);
+
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 99;
+
+  &:hover {
+    opacity:1;
+  }
+`;
+
 const StyledInfoCol = styled(Col)`
   width: 60%;
 `;
